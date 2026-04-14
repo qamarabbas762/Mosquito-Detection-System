4dcaa1bb499aa0b25f013dc28741db88bd7e030b
qamarabbas762 <99537800+qamarabbas762@users.noreply.github.com>
2026-04-15 00:57:29 +0530

    setup updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "mosquito-detection",
+    version = "0.1.0",
+    author="Qamar Abbas",
+    author_email="qamarabbas762@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
